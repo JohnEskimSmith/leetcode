f9812f1b05e63ae2637c7e4d4daafee11925a734
andrew.foma <andrew.foma@gmail.com>
2022-03-14 19:36:40 +0300

    jewels and stones

---
+++ strings/jewels-and-stones.py
@@ -0,0 +1,49 @@
+"""
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+
+
+Example 1:
+
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+
+Example 2:
+
+Input: jewels = "z", stones = "ZZ"
+Output: 0
+
+
+
+Constraints:
+
+    1 <= jewels.length, stones.length <= 50
+    jewels and stones consist of only English letters.
+    All the characters of jewels are unique.
+
+
+"""
+
+def jewels_and_stones(jewels: str, stones: str) -> int:
+    return sum(1 for s in stones if s in jewels)
+
+
+if __name__ == '__main__':
+    result = jewels_and_stones(jewels="z", stones="ZZ")
+    print(result)
+
+
+"""
+Time Submitted
+	
+Status
+	
+Runtime
+	
+Memory
+	
+Language
+03/14/2022 19:32	Accepted	24 ms	13.9 MB	python3
+"""
